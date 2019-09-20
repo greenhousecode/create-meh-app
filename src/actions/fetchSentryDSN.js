a6521719f444bc8fc4154267a21b0b828de72307
Shane <shane.van.den.bogaard@greenhousegroup.com>
2019-09-20 16:06:09 +0200

    rebased feature onto develop

---
+++ src/actions/fetchSentryDSN.js
@@ -0,0 +1,53 @@
+/* eslint-disable consistent-return */
+const { ui } = require('inquirer');
+const chalk = require('chalk');
+
+const curlPromise = require('../utils/curlPromise');
+
+const BASE_URL = 'https://sentry.io/api/0';
+
+module.exports = async sentry => {
+  if (!sentry) {
+    return {};
+  }
+
+  const bar = new ui.BottomBar();
+  bar.updateBottomBar(chalk.gray('Fetching Sentry DSN…'));
+
+  try {
+    const { id, slug, org, token } = sentry;
+
+    const url = `${BASE_URL}/projects/${org}/${slug}/keys/`;
+
+    const [response, data] = await curlPromise(url, 'get', {
+      options: {
+        auth: {
+          bearer: token,
+        },
+      },
+    });
+
+    if ([200, 201, 202].indexOf(response.statusCode) === -1) {
+      const { detail } = JSON.parse(response.body);
+      throw new Error(`${response.statusMessage}: ${detail}`);
+    }
+
+    const [{ dsn }] = data;
+
+    bar.updateBottomBar('');
+    console.log(chalk.green('✔ Fetched Sentry DSN'));
+
+    return {
+      sentryId: id,
+      sentrySlug: slug,
+      sentryOrg: org,
+      sentryToken: token,
+      sentryDSN: dsn.public,
+    };
+  } catch (err) {
+    bar.updateBottomBar('');
+    console.log(chalk.red(`✘ Fetching Sentry DSN failed (${err.message}):`));
+    console.log(err.description);
+    return process.exit(1);
+  }
+};
---
+++ src/utils/curlPromise.js
@@ -0,0 +1,47 @@
+const curl = require('curl');
+
+/**
+ * Promised curl request
+ *
+ * @param {string} url
+ * @param {'get'|'post'} method
+ * @param {{body: string | *[] | {[key: string]: *}, options: {[key: string]: *}}} param2
+ * @returns {[*, *]} Returns a tuple of response object and data
+ */
+const curlPromise = (url, method, { body, options = {}, json = true } = {}) =>
+  new Promise((resolve, reject) => {
+    const prefilledOptions = Object.assign(
+      options,
+      json
+        ? {
+            headers: {
+              'Content-type': 'application/json',
+            },
+          }
+        : {},
+      options,
+    );
+
+    const curlOpts = [typeof body === 'object' ? JSON.stringify(body) : body, prefilledOptions];
+
+    curl[method](
+      url,
+      ...(method !== 'post' || !body ? curlOpts.splice(0) : curlOpts),
+      (err, response, data) => {
+        if (err) {
+          return reject(err);
+        }
+
+        let output = null;
+        try {
+          output = JSON.parse(output);
+        } catch (e) {
+          output = data;
+        }
+
+        return resolve([response, output]);
+      },
+    );
+  });
+
+module.exports = curlPromise;
